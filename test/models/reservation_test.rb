6571a02d6ea585302d6e6c152359087399a01657
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2022-11-16 00:41:11 +0900

    refactor/IS_009_アノテーションの更新

--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -14,10 +14,6 @@
 #  created_at        :datetime         not null
 #  updated_at        :datetime         not null
 #
-# Indexes
-#
-#  index_reservations_on_email  (email) UNIQUE
-#
 require "test_helper"
 
 class ReservationTest < ActiveSupport::TestCase
