7511fedde5fe7026605978564eda2205b82a2fc7
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2022-11-16 00:41:48 +0900

    feat/IS_009_reservationテーブルにemailカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_29_061431) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_15_152340) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_29_061431) do
     t.datetime "start_time"
     t.string "online_or_offline"
     t.string "confirm_password"
+    t.string "email"
   end
 
 end
