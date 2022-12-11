5b2eddcda5f50290a3b7dda5f53b200b6d077a25
Takamizu-Kenta <kenta.takamizu@potepan.jp>
2022-12-11 18:40:55 +0900

    fix/IS_019_active_adminでログインユーザｰ名が消えてしまっていたため修正

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -235,6 +235,7 @@ ActiveAdmin.setup do |config|
   config.namespace :admin do |admin|
     admin.build_menu :utility_navigation do |menu|
       menu.add label: "サイトへ戻る", url: "/"
+      admin.add_current_user_to_menu  menu
       admin.add_logout_button_to_menu menu
     end
   end
