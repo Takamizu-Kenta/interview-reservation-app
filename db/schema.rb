c50a1de9426d5c706bf2acd06aee1dde1cdfebf7
Kempagne <kenta130712@gmail.com>
2022-12-25 22:44:42 +0900

    fix/IS_043_重複している内容のマイグレーションファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_17_075149) do
   enable_extension "plpgsql"
 
   create_table "admin_users", force: :cascade do |t|
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
     t.string "name"
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
